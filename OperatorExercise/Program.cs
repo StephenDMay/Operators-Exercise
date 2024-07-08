e6a8c6ca5b5a9a8f2da2ec7935dfef2d8ba9f5f0
Stephen-May1_Dell <stephen_may1@dell.com>
2024-07-08 13:02:47 -0400

    exercise completion

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -1,10 +1,39 @@
-﻿namespace OperatorExercise
+﻿
+namespace OperatorExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int a = 17;
+            int b = 4;
+
+            // Addition
+            int sum = a + b;
+            Console.WriteLine($"{a} + {b} is {sum}");
+
+            // Subtraction
+            int difference = a - b;
+            Console.WriteLine($"{a} - {b} is {difference}");
+
+            // Multiplication
+            int product = a * b;
+            Console.WriteLine($"{a} * {b} is {product}");
+
+            // Division
+            int quotient = a / b;
+            int remainder = a % b;
+            Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder}");
+
+            Console.WriteLine("Enter radius of a circle: ");
+            double radius = double.Parse(Console.ReadLine());
+            double area = AreaOfCircle(radius);
+            Console.WriteLine($"The area of the circle with radius {radius} is {area}");
+        }
+
+        public static double AreaOfCircle(double radius)
+        {
+            return Math.PI * Math.Pow(radius, 2);
         }
     }
 }
